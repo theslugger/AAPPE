0fa8a8947cdb7a9f1f4eb723f2970f29dedd104f
theslugger <384535751@qq.com>
2025-02-27 09:24:05 +0000

    fix 1_INDEX_General_UniprotName.py

--- DataPreprocess/1_INDEX_General_UniprotName.py
+++ DataPreprocess/1_INDEX_General_UniprotName.py
@@ -15,8 +15,6 @@ for line in lines:
 UniprotID_WithoutRepeat= list(set(UniprotID_list))
 #print len(UniprotID_WithoutRepeat)
 
-
-
 f = open('../outfiles1/INDEX_General_UniprotName_List.txt','w')
 f.writelines('\n'.join(UniprotID_WithoutRepeat)+'\n')
 f.close()
